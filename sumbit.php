79476e8dd54991d505977cee02e061d748e503ad
125Y <topnikm@gmail.com>
2025-04-22 22:46:47 +0300

    Update and rename login.php to sumbit.php

---
+++ sumbit.php
@@ -0,0 +1,35 @@
+<?php
+header('Content-Type: application/json');
+$db = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', ''); // Укажи свои
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$name = $data['name'];
+$email = $data['email'];
+$phone = $data['phone'];
+$bio = $data['bio'];
+
+$stmt = $db->prepare("SELECT * FROM users WHERE email = ?");
+$stmt->execute([$email]);
+
+if ($stmt->rowCount() > 0) {
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    $stmt = $db->prepare("UPDATE users SET name=?, phone=?, bio=? WHERE email=?");
+    $stmt->execute([$name, $phone, $bio, $email]);
+
+    echo json_encode(['message' => 'Данные обновлены.']);
+} else {
+    
+    $login = uniqid('user_');
+    $password = bin2hex(random_bytes(4)); //пароль сделать
+    $hashed = password_hash($password, PASSWORD_DEFAULT);
+
+    $stmt = $db->prepare("INSERT INTO users (name, email, phone, bio, login, password) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->execute([$name, $email, $phone, $bio, $login, $hashed]);
+
+    echo json_encode([
+        'message' => 'Пользователь зарегистрирован.',
+        'login' => $login,
+        'password' => $password
+    ]);
+}
