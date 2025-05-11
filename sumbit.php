2bbb84976f81145c7fba62afd83d4e015f235b0f
125Y <topnikm@gmail.com>
2025-05-11 15:55:03 +0300

    Update sumbit.php

--- sumbit.php
+++ sumbit.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(405);
+    
     echo json_encode(['error' => 'Метод не разрешен']);
     exit;
 }
@@ -16,18 +16,20 @@ $message = trim($_POST['message'] ?? '');
 
 // Валидация
 $errors = [];
+
+
 if (empty($name)) $errors['name'] = 'Имя обязательно';
+if (!preg_match('/^([а-яё]+-?[а-яё]+)( [а-яё]+-?[а-яё]+){1,2}$/Diu', $name))  $errors['name'] = 'Можно использовать только кириллицу'
 if (empty($email)) $errors['email'] = 'Email обязателен';
 elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) $errors['email'] = 'Некорректный email';
 if (empty($message)) $errors['message'] = 'Сообщение обязательно';
 
 if (!empty($errors)) {
-    http_response_code(400);
     echo json_encode(['errors' => $errors]);
     exit;
 }
 
-// Настройки БД
+// Настройка БД
 $user = 'u68790'; 
 $pass = '4247220'; 
 
@@ -80,8 +82,7 @@ try {
 
 } catch (PDOException $e) {
     $pdo->rollBack();
-    http_response_code(500);
-    echo json_encode(['error' => 'Ошибка базы данных: ' . $e->getMessage()]);
+    echo json_encode(['error' => 'Непредвиденная ошибка']);
 }
 
 
