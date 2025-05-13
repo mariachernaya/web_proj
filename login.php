14ecf11f2d33a4b60d810df8f2fb9dfed99118e4
125Y <topnikm@gmail.com>
2025-05-13 15:38:40 +0300

    Update login.php

--- login.php
+++ login.php
@@ -39,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="bootstrap.min.css" />
-    <title>Zadacha 6</title>
+    <title>web_proj</title>
 </head>
 
 <body>
