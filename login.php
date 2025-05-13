96d8f78d021e6f1ddc589472635c6456e7a82e58
125Y <topnikm@gmail.com>
2025-05-13 15:31:42 +0300

    Add files via upload

--- login.php
+++ login.php
@@ -1,41 +1,55 @@
 <?php
-header('Content-Type: application/json');
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+header('Content-Type: text/html; charset=UTF-8');
+session_start();
 
-// Настройки БД
-$user = 'u68790'; 
-$pass = '4247220'; 
+if (!empty($_SESSION['login'])) {
+    header('Location: ./');
+    exit();
+}
 
+$error = '';
 
-try {
-  
-    $pdo = new PDO("mysql:host=localhost;dbname=u68790;charset=utf8", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    $login = $_POST['login'] ?? '';
-    $password = md5($_POST['password'] ?? '');
-    
-    // Поиск пользователя
-    $stmt = $pdo->prepare("
-        SELECT u.id, u.name, u.email, u.message 
-        FROM usersi u
-        JOIN user_credentials uc ON u.id = uc.user_id
-        WHERE uc.login = :login AND uc.password_hash = :password
-    ");
-    $stmt->bindParam(':login', $login);
-    $stmt->bindParam(':password', $password);
-    $stmt->execute();
-    
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-    if ($user) {
-        echo json_encode(['success' => true, 'user' => $user]);
-    } else {
-        echo json_encode(['error' => 'Неверный логин или пароль']);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require ('database.php');
+    $login = $_POST['login'];
+    $password = md5($_POST['password']);
+    try {
+        $stmt = $db->prepare("SELECT id FROM users WHERE login = ? and password = ?");
+        $stmt->execute([$login, $password]);
+        $its = $stmt->rowCount();
+        if ($its) {
+            $uid = $stmt->fetchAll(PDO::FETCH_ASSOC)[0]['id'];
+            $_SESSION['login'] = $_POST['login'];
+            $_SESSION['user_id'] = $uid;
+            header('Location: ./');
+        } else
+            $error = 'Неверный логин или пароль';
+    } catch (PDOException $e) {
+        print ('Error : ' . $e->getMessage());
+        exit();
     }
-    
-} catch (PDOException $e) {
-    echo json_encode(['error' => 'Ошибка базы данных: ' . $e->getMessage()]);
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="bootstrap.min.css" />
+    <title>Zadacha 6</title>
+</head>
+
+<body>
+    <form action="" method="post" class="form">
+        <div class="mess" style="color: red;"><?php echo $error; ?></div>
+        <h2>Авторизация</h2>
+        <div> <input class="input" style="width: 100%;" type="text" name="login" placeholder="Логин"> </div>
+        <div> <input class="input" style="width: 100%;" type="text" name="password" placeholder="Пароль"> </div>
+        <button class="button" type="submit">Войти</button>
+    </form>
+</body>
+
+</html>
