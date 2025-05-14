e2d46296e716197710f5076841cd58f900afb06e
125Y <topnikm@gmail.com>
2025-05-14 20:55:18 +0300

    Update form.js

--- form.js
+++ form.js
@@ -1,25 +1,54 @@
 console.log("form.js подключен");
 $(document).ready(function () {
-  // Отправка формы
-  $('.form').on('submit', function (e) {
-    e.preventDefault(); // Остановить стандартную отправку формы
+  console.log("Документ готов");
 
-    var formData = new FormData(this); // Собираем данные формы
+  const $form = $('.form');
+  console.log("Форма найдена?", $form.length); // Должно быть 1
+
+  $form.on('submit', function (e) {
+    e.preventDefault();
+    console.log("Форма отправлена");
+
+    var formData = new FormData(this);
 
     $.ajax({
       type: 'POST',
-      url: 'form.php', // тот же файл
+      url: 'form.php',
       data: formData,
       processData: false,
       contentType: false,
       success: function (response) {
-        // Заменяем всю форму (снаружи до </form>)
+        console.log("Успешный ответ от сервера");
         const newForm = $(response).find('#form-container').html();
         $('#form-container').html(newForm);
       },
       error: function (xhr, status, error) {
-        alert('Ошибка при отправке формы: ' + error);
+        console.error("Ошибка AJAX", error);
       }
     });
   });
 });
+// $(document).ready(function () {
+//   // Отправка формы
+//   $('.form').on('submit', function (e) {
+//     e.preventDefault(); // Остановить стандартную отправку формы
+
+//     var formData = new FormData(this); // Собираем данные формы
+
+//     $.ajax({
+//       type: 'POST',
+//       url: 'form.php', // тот же файл
+//       data: formData,
+//       processData: false,
+//       contentType: false,
+//       success: function (response) {
+//         // Заменяем всю форму (снаружи до </form>)
+//         const newForm = $(response).find('#form-container').html();
+//         $('#form-container').html(newForm);
+//       },
+//       error: function (xhr, status, error) {
+//         alert('Ошибка при отправке формы: ' + error);
+//       }
+//     });
+//   });
+// });
