bf318132f58652778dd4efa37475b5f73cdb1fdc
125Y <topnikm@gmail.com>
2025-05-15 22:33:50 +0300

    Update form.php

--- test/form.php
+++ test/form.php
@@ -108,15 +108,17 @@
 
 <!-- Изменим кнопку "Выйти" на отдельную форму -->
 <div id="authButtons">
-    <?php if ($log): ?>
-        <button class="button edbut" type="submit">Изменить</button>
-        <form action="index.php" method="post" style="display:inline;">
-            <button class="button" type="submit" name="logout_form">Выйти</button>
-        </form>
-    <?php else: ?>
-        <button class="button" type="submit">Отправить</button>
-        <a class="btnlike" href="login.php">Войти</a>
-    <?php endif; ?>
+    <?php
+    if ($log)
+      echo '<button class="button edbut" type="submit">Изменить</button>';
+    else
+      echo '<button class="button" type="submit">Отправить</button>';
+    if ($log)
+      echo '<button class="button" type="submit" name="logout_form">Выйти</button>';
+    else
+      echo '<a class="btnlike" href="login.php" name="logout_form">Войти</a>';
+    ?>
+  
 </div>
     
     </form>
