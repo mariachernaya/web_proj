3a18b37d5a295ce6dac42006b57e6ea0994c3072
125Y <topnikm@gmail.com>
2025-05-16 23:17:05 +0300

    Update database.php

--- test/database.php
+++ test/database.php
@@ -2,5 +2,4 @@
 	$user = 'u68790'; 
 	$pass = '4247220'; 
 	$db = new PDO('mysql:host=localhost;dbname=u68790', $user, $pass,
-	[PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
-?>
+	[PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
