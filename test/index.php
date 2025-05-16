8e7e1d059ac052002f077869197d69873900cd43
125Y <topnikm@gmail.com>
2025-05-16 22:49:57 +0300

    Update index.php

--- test/index.php
+++ test/index.php
@@ -1,7 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
+error_reporting(0);
+ini_set('display_errors', 0);
 $db;
 include ('database.php');
 //header("Content-Type: text/html; charset=UTF-8");
