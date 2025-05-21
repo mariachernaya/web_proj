3ea28109cca95ac1dc0955c5b06b990640095d9c
mariachernaya <5chernaya5.5@gmail.com>
2025-05-21 11:54:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -174,7 +174,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     echo json_encode($response);
     exit();
 } else {
-    header('Location: #form-anchor');
+    header('Location: index.php');
     exit();
 }
 } else {
